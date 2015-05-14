9c9618421bee7c0c23e629745234c709b608506e
Issac <aducode@126.com>
2015-05-14 13:48:28 +0800

    init coroutine

---
+++ coroutine/coroutine.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+__author__ = 'aducode@126.com'
+
+
+class Coroutine(object):
+    """
+    协程类
+    """
+    def __init__(self):
+        """
+        构造方法
+        :return:
+        """
