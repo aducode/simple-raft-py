5930f8c2d739be121af0e1beaaedc46fbd3d08cf
Issac <aducode@126.com>
2015-05-15 10:32:28 +0800

    test generator next() send()

--- coroutine/test.py
+++ coroutine/test.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 __author__ = 'Administrator'
-
+import types
 
 def coroutine():
     # 调用next后，next返回hello
@@ -14,10 +16,54 @@ def coroutine():
     # 然后应该执行到yield xxx 但是没有yield 抛出StopIteration异常
 
 
+def home(title, name):
+    head_gen = head(title)
+    body_gen = body(name)
+    msg = yield '<html>%s%s</html>' % (head_gen.next(), body_gen.next())
+    # print 'home msg:', msg
+    # ret1 = head_gen.send(msg)
+    # ret2 = body_gen.send(msg)
+    # yield ' '.join(['success', ret1, ret2])
+    while True:
+        if isinstance(msg, types.TupleType) and len(msg) == 2:
+            msg = yield '<html>%s%s</html>' % (head_gen.send(msg[0]), body_gen.send(msg[1]))
+        else:
+            msg = yield '<html>%s%s</html>' % (head_gen.next(), body_gen.next())
+
+
+
+def head(title):
+    msg = title
+    msg = yield '<head><title>%s</title></head>' % msg
+    # print 'head msg:', msg
+    # yield 'head success'
+    while True:
+        if not msg:
+            msg = title
+        msg = yield '<head><title>%s</title></head>' % msg
+
+
+def body(name):
+    msg = name
+    msg = yield '<body><h1>%s</h1></body>' % msg
+    # print 'body msg:', msg
+    # yield 'body success'
+    while True:
+        if not msg:
+            msg = name
+        msg = yield '<body><h1>%s</h1></body>' % msg
+
+
 if __name__ == '__main__':
-    g = coroutine()
-    print g.next()  # 第一次必须调用next
-    print g.send("world")
-    g.send('yooooo')
+    # g = coroutine()
+    # print g.next()  # 第一次必须调用next
+    # print g.send("world")
+    # g.send('yooooo')
+    home_gen =  home('hello world', 'Issac')
+    print home_gen.next()
+    print home_gen.send(('hehehehe', 'Alion'))
+    print home_gen.next()
+    print home_gen.send(('fuck', 'FUCKFUCKFUCK'))
+
 
 
